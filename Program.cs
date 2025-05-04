8288aae8ed70dfcac572f8a952ea65c6d02be8e5
Black-Baron-kz <kairatxxxxx@gmail.com>
2025-05-04 22:14:50 +0500

    Information
    Это просто код (файлы и проект), только в качестве заглушки. В последующем будет изменено.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace Console_Lesson_01
+{
+   
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Tom";
+            int age = 34;
+            double height = 1.7;
+            //Здесь используется интерполяция строк
+            Console.WriteLine($"Имя: {name} Возраст: {age} Рост: {height}м");
+            //В этом случае {0}, {1}, {2} являются плейсхолдерами
+            Console.WriteLine("Имя: {0} Возраст: {1} Рост: {2}м", name, age, height);
+
+            Console.ReadKey();
+        }
+    }
+}
